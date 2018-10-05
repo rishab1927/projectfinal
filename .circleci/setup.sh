767a5c2e0f0ef6e317d6af9b325ad0e289d1bece
Rishab <raina007rishab@gmail.com>
2018-10-05 17:58:50 +0530

    permission fix

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -1,5 +1,4 @@
 sudo docker-compose build
-sudo docker-compose up
 sudo $(aws ecr get-login --no-include-email --region us-east-2)
 sudo docker build -t project_final .
 sudo docker tag project_final:latest 610767216559.dkr.ecr.us-east-2.amazonaws.com/project_final:latest
